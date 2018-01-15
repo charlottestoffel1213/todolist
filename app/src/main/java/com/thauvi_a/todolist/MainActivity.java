ace4d35a82c7a96f505e687d7256c69e4ea5fada
alexandre thauvin <alexandre.thauvin@epitech.eu>
2018-01-15 17:14:08 +0100

    import & strings

--- app/src/main/java/com/thauvi_a/todolist/MainActivity.java
+++ app/src/main/java/com/thauvi_a/todolist/MainActivity.java
@@ -17,7 +17,6 @@ import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
