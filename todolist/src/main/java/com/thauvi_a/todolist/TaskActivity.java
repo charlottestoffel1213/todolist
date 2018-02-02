2344f7bd4b4a98797e6c92d0c5ecd0c0ec577cac
alexandrethauvin <alexandre.thauvin@epitech.eu>
2018-02-02 15:38:59 +0100

    add peeker date & time detail task

--- todolist/src/main/java/com/thauvi_a/todolist/TaskActivity.java
+++ todolist/src/main/java/com/thauvi_a/todolist/TaskActivity.java
@@ -14,6 +14,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 import java.util.Calendar;
 
 
@@ -29,7 +31,7 @@ public class TaskActivity extends AppCompatActivity {
     private String key;
     private DatabaseReference mDatabase;
     private EditText name;
-    private EditText date;
+    private TextView date;
     private EditText desc;
 
     @Override
