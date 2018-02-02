f21ca5b3757a174f985e9f44a1fd0d201ad6bce6
alexandrethauvin <alexandre.thauvin@epitech.eu>
2018-02-02 15:56:33 +0100

    can edit date & time

--- todolist/src/main/java/com/thauvi_a/todolist/TaskActivity.java
+++ todolist/src/main/java/com/thauvi_a/todolist/TaskActivity.java
@@ -1,12 +1,20 @@
 package com.thauvi_a.todolist;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -14,9 +22,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 
 /**
@@ -32,6 +42,7 @@ public class TaskActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private EditText name;
     private TextView date;
+    private TextView time;
     private EditText desc;
 
     @Override
@@ -70,6 +81,75 @@ public class TaskActivity extends AppCompatActivity {
         });
     }
 
+    public void retrieveFieldsValue() {
+        date = findViewById(R.id.date);
+        time = findViewById(R.id.time);
+        name = findViewById(R.id.name);
+        desc = findViewById(R.id.desc);
+    }
+
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            TextView textView = getActivity().findViewById(R.id.time);
+            StringBuilder formattedTime = new StringBuilder().append(hourOfDay)
+                    .append(":")
+                    .append(minute < 10 ? "0" + minute : minute);
+            textView.setText(formattedTime);
+        }
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            TextView textView = getActivity().findViewById(R.id.date);
+            StringBuilder formattedDate = new StringBuilder().append(year)
+                    .append("-")
+                    .append(month + 1 < 10 ? "0" + (month + 1) : month + 1)
+                    .append("-")
+                    .append(day + 1 < 10 ? "0" + (day + 1) : day);
+            textView.setText(formattedDate);
+        }
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -83,13 +163,10 @@ public class TaskActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        retrieveFieldsValue();
         //noinspection SimplifiableIfStatement
 
         if (id == R.id.done) {
-            name = findViewById(R.id.name);
-            desc = findViewById(R.id.desc);
-            date = findViewById(R.id.date);
             mDatabase.child(key).removeValue();
             mDatabase = FirebaseDatabase.getInstance().getReference().child("Finished");
             DatabaseReference oldTask = mDatabase.push();
@@ -106,11 +183,24 @@ public class TaskActivity extends AppCompatActivity {
         }
 
         else if (id == R.id.save) {
-            name = findViewById(R.id.name);
-            desc = findViewById(R.id.desc);
-            date = findViewById(R.id.date);
+            boolean dateIsEmpty = false;
+            SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+            Date dueDate;
+            try {
+                dueDate = dateFormatter.parse(date.getText().toString() + " " + time.getText().toString());
+            } catch (ParseException e) {
+                try {
+                    dueDate = dateFormatter.parse("2016-01-05 05:05");
+                    dateIsEmpty = true;
+                } catch (ParseException f) {
+                    f.getMessage();
+                    return false;
+                }
+            }
             mDatabase.child(key).child("name").setValue(name.getText().toString());
             mDatabase.child(key).child("desc").setValue(desc.getText().toString());
+            if (!dateIsEmpty)
+                mDatabase.child(key).child("date").setValue(dueDate.toString());
             this.finish();
             return true;
         }
